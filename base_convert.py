d3ab75b65e834bd3b37839aec5faaf38102a4606
Aryan <akumar54@calpoly.edu>
2021-01-19 19:56:57 -0800

    fix

--- base_convert.py
+++ base_convert.py
@@ -1,3 +1,16 @@
+import math
+
+numbers = {
+    10: "A",
+    11: "B",
+    12: "C",
+    13: "D",
+    14: "E",
+    15: "F"
+}
+
+
+
 #(int, int) -> number in different base
 def convert(num, b):
     """Recursive function that returns a string representing num in the base b"""
@@ -5,8 +18,8 @@ def convert(num, b):
         final = converter(num, b)
 
         #Checking for special case when empty string is output
-        if final == '':
-            return '0'
+        if final == "":
+            return "0"
         else:
             return final
     
@@ -14,12 +27,13 @@ def convert(num, b):
 #Converts base recursively
 def converter(num, b):
     if num == 0:
-        return ''
+        return ""
 
     remainder = num % b
     q = num // b
+
     if b > 10 and remainder > 9:
-        remainder = chr(remainder + 55) #convering to letters if remainder is in double digits
+        remainder = numbers[remainder] #convering to letters if remainder is in double digits
 
     return str(converter(q, b)) + str(remainder) #recursively call function to have remainders in reverse
     """if final == '':
